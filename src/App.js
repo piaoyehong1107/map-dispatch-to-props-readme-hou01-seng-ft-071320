bca9866d89ed78bca8dfc68d5ef6e3cf3257574a
Yehong Piao <piaoyehong1107@gmail.com>
2020-10-09 04:51:05 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import { addItem } from  './actions/items';
 
 class App extends Component {
 
-  handleOnClick() {
-    this.props.store.dispatch(addItem());
-  }
+ handleOnClick = event => {
+    this.props.addItem()
+ 
 
   render() {
     return (
@@ -26,5 +26,12 @@ const mapStateToProps = (state) => {
     items: state.items
   };
 };
+const mapDispatchToProps = dispatch => {
+  return {
+    addItem: () => {
+      dispatch(addItem())
+    }
+  };
+};
 
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
